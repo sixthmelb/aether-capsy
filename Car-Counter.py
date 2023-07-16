f86cb0d95e09a17b2825d9b17d1fc44139a03efc
Kaizen (改善) <yogijr510@gmail.com>
2023-07-16 20:19:39 +0800

    add line webcam func

--- Car-Counter.py
+++ Car-Counter.py
@@ -6,6 +6,9 @@ import math
 from sort import *
 
 cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Video
+# cap = cv2.VideoCapture(1)  # For Webcam
+# cap.set(3, 1280)
+# cap.set(4, 720)
 
 model = YOLO("../Yolo-Weights/yolov8x.pt")
 
